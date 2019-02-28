5cd3132dd111d262eee5ded284739becf5bf5aac
Vanya Levchenko <vanyalevchenko@Vanyas-MacBook-Pro.local>
2019-02-27 19:02:17 -0700

    Routes added for user authentication.

--- app.js
+++ app.js
@@ -2,20 +2,16 @@ const express = require('express');
 const debug = require('debug')('app')
 const path = require('path');
 const chalk = require('chalk');
+const bodyParser = require('body-parser');
 
 const app = express()
 const port = process.env.port || 3000
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, '/public/')))
-app.use(
-  '/css',
-  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css'))
-)
-app.use(
-  '/js',
-  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js'))
-)
+app.use('/css',express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use('/js',express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery')))
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
@@ -27,10 +23,11 @@ const nav =  [
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
-
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter)
 app.use('/admin', adminRouter)
+app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
   res.render('index', {
